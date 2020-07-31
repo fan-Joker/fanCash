6da997f1bdfd0454cc9a18ffaee8c4b6952558af
Tiago <tiagograveto@outlook.com>
2020-07-31 01:38:32 -0300

    Adicionado sistema de criar uma tabela do jogador, quando ele entra no servidor.

--- src/me/fanjoker/cash/Main.java
+++ src/me/fanjoker/cash/Main.java
@@ -1,12 +1,16 @@
 package me.fanjoker.cash;
 
+import me.fanjoker.cash.listener.PlayerJoin;
 import me.fanjoker.cash.manager.CashConnection;
 import me.fanjoker.cash.config.ConfigManager;
 import me.fanjoker.cash.manager.CashManager;
 import me.fanjoker.cash.others.Runnable;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import me.fanjoker.cash.commands.Cash;
+
+import java.nio.Buffer;
 
 public class Main extends JavaPlugin{
 	
@@ -23,6 +27,7 @@ public class Main extends JavaPlugin{
 		connection = new CashConnection();
 		configManager.loadConfig("config");
 		getCommand("cash").setExecutor(new Cash("cash"));
+		Bukkit.getPluginManager().registerEvents(new PlayerJoin(), this);
 		new Runnable().runTaskTimer(instance, 0, 20 * 60 * 5);
 	}
 
---
+++ src/me/fanjoker/cash/listener/PlayerJoin.java
@@ -0,0 +1,17 @@
+package me.fanjoker.cash.listener;
+
+import me.fanjoker.cash.Main;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+
+public class PlayerJoin implements Listener {
+
+    @EventHandler
+    public void onJoin(PlayerJoinEvent e) {
+        String name = e.getPlayer().getName();
+        if (!Main.getManager().existsPlayer(name)) {
+            Main.getManager().createAccount(name);
+        }
+    }
+}
